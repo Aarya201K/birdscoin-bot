04b1dbb0ca4f8f28c47484acbec10d0efe2c2190
Aarya Demo <169067634+Aarya201K@users.noreply.github.com>
2026-06-14 11:42:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
-print("Birdscoin Bot Running")
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+from config import BOT_TOKEN
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🕊️ Welcome to Birdscoin (BRDC)"
+    )
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(
+    CommandHandler("start", start)
+)
+
+print("Birdscoin Bot Started")
+
+app.run_polling()
